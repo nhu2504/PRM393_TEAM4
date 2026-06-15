015f1489af13da26d4629dbe6b8c549d7bfb0ba4
nhu2504 <vannhu25042004@gmail.com>
2026-06-15 12:17:24 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-<<<<<<< Updated upstream
 import 'views/main_screen.dart';
-=======
-
-import 'views/main_screen.dart';
-import 'screens/ticket_detail_screen.dart';
->>>>>>> Stashed changes
 
 void main() {
   runApp(const MovieTicketApp());
@@ -20,43 +14,20 @@ class MovieTicketApp extends StatelessWidget {
     return MaterialApp(
       title: 'Đặt vé xem phim',
       debugShowCheckedModeBanner: false,
-<<<<<<< Updated upstream
       scrollBehavior: AppScrollBehavior(),
-=======
-
-      // Giữ ScrollBehavior của bạn kia
-      scrollBehavior: AppScrollBehavior(),
-
-      // Giữ Theme của bạn kia
->>>>>>> Stashed changes
       theme: ThemeData(
         primarySwatch: Colors.red,
         scaffoldBackgroundColor: Colors.grey[100],
         fontFamily: 'Roboto',
         useMaterial3: true,
       ),
-<<<<<<< Updated upstream
       home: const MainScreen(),
-=======
-
-      // Giữ Route của bạn
-      initialRoute: '/',
-
-      routes: {
-        '/': (context) => const MainScreen(),
-        '/ticket_detail': (context) => const TicketDetailScreen(),
-      },
->>>>>>> Stashed changes
     );
   }
 }
 
-<<<<<<< Updated upstream
 /// Custom ScrollBehavior cho phép kéo bằng chuột/trackpad
 /// (mặc định Flutter chỉ hỗ trợ kéo bằng touch/stylus)
-=======
-/// Cho phép kéo bằng chuột, touchpad, bút cảm ứng,...
->>>>>>> Stashed changes
 class AppScrollBehavior extends MaterialScrollBehavior {
   @override
   Set<PointerDeviceKind> get dragDevices => {
